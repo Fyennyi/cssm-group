a9e3b3a2c846cb18437ef4082be781b14fa0c8d0
Sergiy Chernega <chernegasergiy3@gmail.com>
2024-07-31 23:26:40 +0300

    Add files via upload

---
+++ src/context/TranslationContext.js
@@ -0,0 +1,16 @@
+﻿import React, { createContext, useContext } from 'react';
+import translations from '../translations';
+
+const TranslationContext = createContext();
+
+export const useTranslation = () => useContext(TranslationContext);
+
+export const TranslationProvider = ({ children, language }) => {
+  const translate = (key) => translations[language][key] || key;
+
+  return (
+    <TranslationContext.Provider value={{ translate, language }}>
+      {children}
+    </TranslationContext.Provider>
+  );
+};
