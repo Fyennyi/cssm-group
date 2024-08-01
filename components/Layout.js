5d0e6e65ed9cb26b3982bba72227501c64a985fd
Sergiy Chernega <chernegasergiy3@gmail.com>
2024-08-01 16:40:15 +0300

    Add files via upload

--- components/Layout.js
+++ components/Layout.js
@@ -1,18 +1,13 @@
 ﻿import Head from 'next/head'
 import CookieNotice from './CookieNotice'
 
-export default function Layout({ children, t, pageType }) {
+export default function Layout({ children, t }) {
   return (
     <>
       <Head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>CSSM Group</title>
-        {pageType === 'main' ? (
-          <link rel="stylesheet" href="/styles/globals.css" />
-        ) : (
-          <link rel="stylesheet" href="/styles/article.css" />
-        )}
       </Head>
 
       {children}
