862c8755ae45a78c3edec5294d12078187807359
Sergiy Chernega <chernegasergiy3@gmail.com>
2024-08-01 18:28:37 +0300

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,7 @@ export default function Home() {
             <div className="content">
               <h1>{t('section1-title')}</h1>
               <p>{t('section1-subtitle')}</p>
-              <a href="#section2">{t('section1-button')}</a>
+              <a href="#section2" className="btn">{t('section1-button')}</a>
             </div>
           </section>
 
@@ -43,7 +43,7 @@ export default function Home() {
                   <li key={index}>{t(`section3-${item}`)}</li>
                 ))}
               </ul>
-              <a href="#section5">{t('section3-button')}</a>
+              <a href="#section5" className="btn">{t('section3-button')}</a>
             </div>
           </section>
 
@@ -53,7 +53,7 @@ export default function Home() {
             <div className="content">
               <h2>{t('section5-title')}</h2>
               <p>{t('section5-content')}</p>
-              <a href="#">{t('section5-button')}</a>
+              <a href="#" className="btn">{t('section5-button')}</a>
             </div>
           </section>
         </main>
