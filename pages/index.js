9957a02a4df234a5a744d62f248c59e749b8e0e2
Sergiy Chernega <chernegasergiy3@gmail.com>
2024-08-02 19:39:58 +0300

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-﻿import { useState } from 'react'
+﻿import { useState, useEffect } from 'react'
 import Head from 'next/head'
 import Layout from '../components/Layout'
 import Header from '../components/Header'
