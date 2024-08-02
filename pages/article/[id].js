243197f9d2b76565074611d2801223cb4d72fa84
Sergiy Chernega <chernegasergiy3@gmail.com>
2024-08-02 15:45:20 +0300

    Add files via upload

--- pages/article/[id].js
+++ pages/article/[id].js
@@ -5,6 +5,8 @@ import Layout from '../../components/Layout'
 import { useTranslation } from '../../lib/translations'
 import Cookies from 'js-cookie'
 
+import('../styles/article.css')
+
 const articles = [
   {
     id: 'cubecraft',
@@ -111,21 +113,6 @@ export default function Article({ article }) {
   const router = useRouter()
   const [lang, setLang] = useState(Cookies.get('language') || 'uk')
   const { t } = useTranslation(lang)
-
-  if (!article) {
-    return (
-      <Layout lang={lang} setLang={setLang} t={t}>
-        <Head>
-          <title>{t('site-title')}</title>
-        </Head>
-        <div className="error-container">
-          <h1 className="error-title">{t('article-error-title')}</h1>
-          <p className="error-message">{t('article-error-message')}</p>
-          <button onClick={() => router.push('/')} className="error-button">{t('article-error-button')}</button>
-        </div>
-      </Layout>
-    )
-  }
 
   return (
     <Layout lang={lang} setLang={setLang} t={t}>
