587a0d365fe3d7c092831e410edb78c695cfca5c
Sergiy Chernega <chernegasergiy3@gmail.com>
2025-02-18 17:33:57 +0200

    Update PortfolioGrid.js

--- components/PortfolioGrid.js
+++ components/PortfolioGrid.js
@@ -1,4 +1,4 @@
-﻿import { useState, useEffect } from 'react';
+﻿import { React, useState, useEffect } from 'react';
 import articles from '../data/articles';
 
 export default function PortfolioGrid({ t }) {
