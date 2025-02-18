e8dee53ba96a258bdaf689a50fd7b0cdcde54426
Sergiy Chernega <chernegasergiy3@gmail.com>
2025-02-18 17:36:04 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-﻿import { useState, useEffect } from 'react'
+﻿import { React, useState, useEffect } from 'react'
 import Head from 'next/head'
 import Layout from '../components/Layout'
 import Header from '../components/Header'
