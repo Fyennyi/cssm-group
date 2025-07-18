6ee089c50bdf1080773894f8e0a85281590ea66a
Sergiy Chernega <chernegasergiy3@gmail.com>
2025-07-18 19:38:19 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production';
+// The repository name is passed from the GitHub Actions workflow.
+const repoName = process.env.REPOSITORY_NAME;
+
 const nextConfig = {
-  output: 'export'
+  output: 'export',
+  // Required for GitHub Pages, since the site is hosted at a subpath.
+  basePath: isProd && repoName ? `/${repoName}` : '',
+  assetPrefix: isProd && repoName ? `/${repoName}/` : '',
 };
 
 module.exports = nextConfig;
