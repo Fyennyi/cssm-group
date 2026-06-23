3f233113e82215040fa9264d03aad7bb4591bb63
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-06-23 21:23:50 +0300

    fix: Revert next/image back to img to fix production build

--- pages/article/[id].js
+++ pages/article/[id].js
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import { useRouter } from 'next/router';
 import Head from 'next/head';
-import Image from 'next/image';
 import fs from 'node:fs';
 import path from 'node:path';
 import { MDXRemote } from 'next-mdx-remote';
@@ -82,7 +81,7 @@ export default function Article({ article, mdxSource, translations }) {
             <div className={styles.articleSlider}>
               {article.relatedProjects.map((project, _index) => (
                 <div key={project.id || project.title} className={styles.sliderItem}>
-                  <Image src={`${router.basePath}/img/${project.image}`} alt={project.title} width={400} height={300} style={{ objectFit: 'cover', borderRadius: '8px' }} />
+                  <img src={`${router.basePath}/img/${project.image}`} alt={project.title} width={400} height={300} style={{ objectFit: 'cover', borderRadius: '8px' }} />
                   <h4>{project.title}</h4>
                   <p>{project.description}</p>
                 </div>
